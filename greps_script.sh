ec3a0b5f3ba1375d6fdfe7bb776cac49156e3252
stevelacerda7 <stevelacerda7@gmail.com>
2021-12-02 10:07:12 -0800

    Update greps_script.sh
    Added iostat function and removed threads. iostat is specific to netapp, but doesn't run if it doesn't find an iostat file anyhow.

--- greps_script.sh
+++ greps_script.sh
@@ -28,6 +28,7 @@ tombstone_file="Nibbler/1-tombstones.out"
 histograms="Nibbler/1-histograms.out"
 drops="Nibbler/1-drops.out"
 queues="Nibbler/1-queues.out"
+iostat="Nibbler/1-iostat"
 hash_line="=========================================================================================================="
 
 
@@ -136,14 +137,14 @@ function solr() {
 	echo "Solr greps" > $solr_file
 
 	echo_request "SOLR DELETES" $solr_file
-	egrep -iRc 'ttl.*scheduler.*expired' ./ --include={system,debug}* | egrep "[1-9]" >> $solr_file
+	egrep -iRc 'ttl.*scheduler.*expired' ./ --include={system,debug}* | egrep ":[1-9]" >> $solr_file
 
 	h=`egrep -iRh 'max_docs_per_batch' ./ --include=dse.yaml | head -1 | awk '{print $2}'`
 	echo_request "SOLR DELETES HITTING $h THRESHOLD" $solr_file
-	egrep -icR "ttl.*scheduler.*expired.*$h" ./ --include={system,debug}* | egrep "[1-9]" >> $solr_file
+	egrep -icR "ttl.*scheduler.*expired.*$h" ./ --include={system,debug}* | egrep ":[1-9]" >> $solr_file
 
 	echo_request "SOLR AUTOCOMMIT" $solr_file
-	egrep -icR 'commitScheduler.*DocumentsWriter' ./ --include={system,debug}* | egrep "[1-9]" >> $solr_file
+	egrep -icR 'commitScheduler.*DocumentsWriter' ./ --include={system,debug}* | egrep ":[1-9]" >> $solr_file
 
 	echo_request "SOLR COMMITS BY CORE" $solr_file
 	egrep -iR 'AbstractSolrSecondaryIndex.*Executing soft commit' ./ --include={system,debug}* | awk '{print $1,$(NF)}' | sort | uniq -c >> $solr_file
@@ -155,7 +156,7 @@ function solr() {
 	egrep -iR 'Index WorkPool.Lucene flush' ./ --include={system,debug}* | awk -F'[' '{print $2}' | awk '{print $1}' | sort | uniq -c >> $solr_file
 
 	echo_request "SOLR FLUSHES BY THREAD" $solr_file
-	egrep -iR 'SolrMetricsEventListener.*Lucene flush' ./ --include={system,debug}* | awk -F']' '{print $1}' | awk -F'[' '{print $2}' | sed 's/:.*//g' | sed 's/[0-9]*//g' | sed 's/\-/ /g'|  sort | uniq -c >> $solr_file
+	egrep -iR 'SolrMetricsEventListener.*Lucene flush' ./ --include={system,debug}* | awk -F']' '{print $1}' | awk -F'[' '{print $2}' | sed 's/:.*//g' | sed 's/[0-9]*//g' | sed 's/\-/ /g'|  sort | uniq -c | sort >> $solr_file
 
 	echo_request "SOLR FLUSH SIZE" $solr_file
 	echo "0 - 999kB" >> $solr_file
@@ -183,7 +184,7 @@ function solr() {
 
 	echo_request "LUCENE MERGES" $solr_file
 	echo "total lucene merges" >> $solr_file
-	grep -ciR "Lucene merge" ./ --include={system,debug}* | egrep "[1-9]" >> $solr_file
+	grep -ciR "Lucene merge" ./ --include={system,debug}* | egrep ":[1-9]" >> $solr_file
 
 	echo >> $solr_file
 	echo "100ms - 249ms" >> $solr_file
@@ -243,19 +244,19 @@ function greps() {
 	touch $histograms
 
 	echo_request "DROPPED MESSAGES" 
-	egrep -icR 'DroppedMessages.java' ./ --include={system,debug}* | egrep "[1-9]" >> $grep_file
+	egrep -icR 'DroppedMessages.java' ./ --include={system,debug}* | egrep ":[1-9]" | awk -F: '{print $1,$2}' | sort -k2 -r -h | column -t >> $grep_file
 
 	echo_request "POSSIBLE NETWORK ISSUES" 
-	grep -ciR 'Unexpected exception during request' ./ --include={system,debug}* | egrep "[1-9]" >> $grep_file
+	grep -ciR 'Unexpected exception during request' ./ --include={system,debug}* | egrep ":[1-9]" | awk -F: '{print $1,$2}' | sort -k2 -r -h | column -t >> $grep_file
 	
 	echo_request "HINTED HANDOFFS TO ENDPOINTS" 
-	grep -R 'Finished hinted handoff' ./ --include={system,debug}* | awk -F'endpoint' '{print $2}' | awk '{print $1}' | sort | uniq -c  >> $grep_file
+	grep -R 'Finished hinted handoff' ./ --include={system,debug}* | awk -F'endpoint' '{print $2}' | awk '{print $1}' | sort | uniq -c | sort >> $grep_file
 
 	# echo_request "COMMIT-LOG-ALLOCATE FLUSHES - TODAY" 
 	# egrep -ciR 'commit-log-allocator.*$today.*enqueuing' ./ --include={debug,output}* | sort -k 1 | awk -F':' '{print $1,$2}' | column -t >> $grep_file
 
 	echo_request "FLUSHES BY THREAD" 
-	egrep -iRh 'enqueuing flush of' ./ --include={system,debug}* | awk -F']' '{print $1}' | awk -F'[' '{print $2}' | sed 's/:.*//g' | awk -F'(' '{print $1}' | awk -F'-' '{print $1}' | sort | uniq -c >> $grep_file
+	egrep -iRh 'enqueuing flush of' ./ --include={system,debug}* | awk -F']' '{print $1}' | awk -F'[' '{print $2}' | sed 's/:.*//g' | awk -F'(' '{print $1}' | awk -F'-' '{print $1}' | sort | uniq -c | sort >> $grep_file
 	# echo_request "FLUSHES BY THREAD - TODAY" 
 	# egrep -iRh '$today.*enqueuing flush of' ./ --include={system,debug}* | awk -F']' '{print $1}' | awk -F'[' '{print $2}' | sed 's/:.*//g' | sort | uniq -c >> $grep_file
 
@@ -272,7 +273,7 @@ function greps() {
 	# echo_request "AVERAGE FLUSH SIZE" 
 	# egrep -iR 'enqueuing flush of' ./ --include={system,debug}* | awk -F'Enqueuing' '{print $2}' | awk -F':' '{print $2}' | column -t | awk 'BEGIN {p=1}; {for (i=1; i<=NF;i++) total = total+$i; p=p+1}; END {print sprintf("%.0f", total/p)}' | awk '{ byte =$1 /1024/1024; print byte " MB" }' >> $grep_file
 	echo_request "TOTAL COMPACTIONS" 
-	egrep -ciR 'Compacted' ./ --include={system,debug}* | sort -k 1 | egrep "[1-9]" >> $grep_file
+	egrep -ciR 'Compacted' ./ --include={system,debug}* | sort -k 1 | egrep ":[1-9]" | awk -F: '{print $1,$2}' | sort -k2 -r -h | column -t >> $grep_file
 
 	# echo_request "TOTAL COMPACTIONS IN LAST DAY" 
 	# egrep -ciR '$today.*Compacted' ./ --include={system,debug}* | sort -k 1 >> $grep_file
@@ -290,17 +291,17 @@ function greps() {
 	# measures the longest compaction times with node info
 	# .//nodes/10.36.27.157/logs/cassandra/debug.log	5,829,323ms
 	echo_request "LONGEST COMPACTION TIMES WITH NODE INFO"
-	egrep -R "Compacted\ \(" ./ --include=debug.log | egrep -o ".*?\,.*\dms"  | awk '{print $1,$NF}' | sed 's/:.* /\t/g' | sort -h -r -k2 | head -20 >> $grep_file
+	egrep -R "Compacted\ \(" ./ --include=debug.log | egrep -o ".*?\,.*\dms"  | awk '{print $1,$NF}' | sed 's/:.* /\t/g' | sort -h -r -k2 | head -20 | sort -k2 -r -h | column -t >> $grep_file
 
 	echo_request "RATE LIMITER APPLIED"
 	echo "Usually means too many operations, check concurrent reads/writes in c*.yaml" >> $grep_file
 	egrep -R "RateLimiter.*currently applied" ./ --include={system,debug}* >> $grep_file
 
 	echo_request "GC - OVER 100ms" 
-	egrep -ciR 'gc.*\d\d\dms' ./ --include={system,debug}* | awk -F':' '($2>0){print $1,$2,$3}' | sort -k 1 >> $grep_file
+	egrep -ciR 'gc.*\d\d\dms' ./ --include={system,debug}* | awk -F':' '($2>0){print $1,$2,$3}' | sort -k 1 | awk -F: '{print $1,$2}' | sort -k2 -r -h | column -t >> $grep_file
 
 	echo_request "GC - OVER 100ms TODAY" 
-	egrep -ciR '$(date +%Y-%m-%d).*gc.*\d\d\dms' ./ --include={system,debug}* | awk -F':' '($2>0){print $1,$2,$3}' | sort -k 1 >> $grep_file
+	egrep -ciR '$(date +%Y-%m-%d).*gc.*\d\d\dms' ./ --include={system,debug}* | awk -F':' '($2>0){print $1,$2,$3}' | sort -k 1 | awk -F: '{print $1,$2}' | sort -k2 -r -h | column -t >> $grep_file
 
 	echo_request "GC - GREATER THAN 1s" 
 	egrep -ciR 'gc.*\d\d\d\dms' ./ --include={system,debug}* | awk -F':' '($2>0){print $1,$2,$3}' | sort -k 1 >> $grep_file
@@ -318,7 +319,7 @@ function greps() {
 	egrep -iRh 'tombstone' ./ --include={system,debug}*  | grep -o 'scanned over.*\|live rows and.*' | awk '{print $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13}' | awk '{for (I=1;I<NF;I++) if ($I == "FROM") print $2, $(I+1)}' | sort -nrk1 | awk '!seen[$2]++' >> $grep_file
 
 	echo_request "TOMBSTONE ALERTS BY NODE"
-	egrep -ciR 'tombstone' ./ --include={system,debug}* | egrep "[1-9]" >> $grep_file
+	egrep -ciR 'tombstone' ./ --include={system,debug}* | egrep ":[1-9]" | awk -F: '{print $1,$2}' | sort -k2 -r -h | column -t >> $grep_file
 
 	echo_request "TOMBSTONE QUERY ABORTS BY TABLE"
 	egrep -iRh 'tombstone' ./ --include={system,debug}* | grep "aborted" | awk '{for (I=1;I<NF;I++) if ($I == "FROM") print $(I+1)}' | sort | uniq -c >> $grep_file
@@ -342,7 +343,7 @@ function greps() {
 	fi
 
 	echo_request "MERGED COMPACTIONS COUNT"
-	egrep -Ric 'Compacted (.*).*]' ./ --include={debug,system}* | egrep "[1-9]" >> $grep_file
+	egrep -Ric 'Compacted (.*).*]' ./ --include={debug,system}* | egrep ":[1-9]" | awk -F: '{print $1,$2}' | sort -k2 -r -h | column -t >> $grep_file
 
 	echo_request "MERGED COMPACTIONS" 
 	egrep -R 'Compacted (.*).*]' ./ --include={debug,system}* | awk '$0 ~ /[1-9][0-9]\ sstables/{print $0}' >> $grep_file
@@ -362,7 +363,7 @@ function greps() {
 		egrep -iR 'Launching' ./ --include=opscenterd.log | egrep -o '\d{1,5}.*time to complete' | cut -d' ' -f5-60 | sort | uniq >> $grep_file
 
 		echo_request "NTP" 
-		egrep -iR 'time correct|exit status' ./ --include=ntpstat >> $grep_file
+		egrep -iR 'time correct|exit status' ./ --include=ntpstat | awk -F: '{print $1,$2}' | sort -k2 -r -h | column -t >> $grep_file
 
 		echo_request "LCS TABLES"
 		egrep -iR "create table|and compaction" $driver_file --include=schema| grep -B1 "LeveledCompactionStrategy" >> $grep_file
@@ -376,13 +377,13 @@ function greps() {
 	fi
 
 	echo_request "PREPARED STATEMENTS DISCARDED"
-	egrep -Rc "prepared statements discarded" ./ --include={system,debug}* | egrep "[1-9]" >> $grep_file
+	egrep -Rc "prepared statements discarded" ./ --include={system,debug}* | egrep ":[1-9]" | awk -F: '{print $1,$2}' | sort -k2 -r -h | column -t >> $grep_file
 
 	echo_request "AGGREGATION QUERY USED WITHOUT PARTITION KEY"
-	egrep -ciR 'Aggregation query used without partition key' ./ --include={system,debug}* | egrep "[1-9]" >> $grep_file
+	egrep -ciR 'Aggregation query used without partition key' ./ --include={system,debug}* | egrep ":[1-9]" | awk -F: '{print $1,$2}' | sort -k2 -r -h | column -t >> $grep_file
 
 	echo_request "CHUNK CACHE ALLOCATION"
-	egrep -ciR "Maximum memory usage reached.*cannot allocate chunk of" ./ --include={system,debug}* | egrep "[1-9]"  >> $grep_file
+	egrep -ciR "Maximum memory usage reached.*cannot allocate chunk of" ./ --include={system,debug}* | egrep ":[1-9]" | awk -F: '{print $1,$2}' | sort -k2 -r -h | column -t  >> $grep_file
 
 	echo_request "ERRORS" $error
 	egrep -R "ERROR" ./ --include={system,debug}* >> $error
@@ -390,9 +391,9 @@ function greps() {
 	echo_request "WARN" $warn
 	egrep -R "WARN" ./ --include={system,debug}* >> $warn
 
-	echo_request "THREADS" $threads
-	echo "All threads from system and debug logs" >> $threads
-	egrep -R ".*" ./ --include={system,debug}* | awk -F'[' '{print $2}' | awk -F']' '{print $1}' | sed 's|:.*||g' | sed 's|[(#].*||g' | sed 's/Repair-Task.*/Repair-Task/g' | sort -k2 | uniq -c | sort -k1 -n >> $threads
+	# echo_request "THREADS" $threads
+	# echo "All threads from system and debug logs" >> $threads
+	# egrep -R ".*" ./ --include={system,debug}* | awk -F'[' '{print $2}' | awk -F']' '{print $1}' | sed 's|:.*||g' | sed 's|[(#].*||g' | sed 's/Repair-Task.*/Repair-Task/g' | sort -k2 | uniq -c | sort -k1 -n -r >> $threads
 
 	echo_request "DROPPED" $drops
 	echo "All dropped messages (mutation, read, hint)" >> $drops
@@ -406,8 +407,9 @@ function greps() {
 
 	echo_request "Latency waiting in Queue" $queues
 	echo "Track if a queue is high from tpstats. We're looking for anything over 300ms" >> $queues
-	echo "Message type           Dropped                  Latency waiting in queue (micros)                                    
-                                             50%               95%               99%               Max" >> $queues
+	echo "                                Message type           Dropped                  Latency waiting in queue (micros)                                    
+                                                                                50%               95%               99%               Max" >> $queues
+	echo "" >> $queues
 	egrep -R "Latency waiting in queue" -A 20 ./ --include=tpstats | egrep ".*[3-9]\d\d\d\d\d\." >> $queues
 }
 
@@ -416,6 +418,21 @@ function diag-import() {
 	filename=$1
 	python3 ~/Downloads/1-scripts/diag-import-main/import $filename
 	python3 ~/Downloads/1-scripts/diag-viewer-main/app.py "$filename/diagnostics.db"
+}
+
+function iostat() {
+	i=0
+	for f in `find ./ -name iostat`;
+		do 
+			if [ $i -eq 0 ]
+			then
+				touch $iostat
+				echo > $iostat
+			fi
+			i=1
+			echo $f >> $iostat
+			sperf sysbottle $f >> $iostat
+		done
 }
 
 function use_options() {
@@ -479,6 +496,7 @@ while true; do
 		config
 		solr
 		greps
+		iostat
 		# sixO
 		break
 		;;
